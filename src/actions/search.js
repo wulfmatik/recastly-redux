a68c98520124c047b8710243777b52691fc8b2a3
Blake Crawford <blake.crawford@outlook.com>
2022-07-18 20:47:38 -0700

    completed currentvideo and videolist action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,14 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+
   //TODO:  Write an asynchronous action to handle a video search!
+  var callback = (videos) => {
+    changeVideoList(videos);
+    changeVideo(videos[0]);
+  };
+
+  searchYouTube({YOUTUBE_API_KEY, q}, callback);
 };
 
 export default handleVideoSearch;
