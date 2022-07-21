b067c3c8806983740eb98c2e843db447f345bcba
Blake Crawford <blake.crawford@outlook.com>
2022-07-20 20:37:53 -0700

    Complete connecting combine reducer to store

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,15 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
-  //TODO: define a reducer for the currentVideo field of our state.
+var currentVideoReducer = (state = null, action) => {
+  //TODO: define a reducer for the currentVideo field of our state.c
+  switch (action.type) {
+  case 'CHANGE_VIDEO':
+    console.log(action);
+    return action.video;
+
+  default:
+    return state;
+  }
 };
 
 export default currentVideoReducer;
--- src/store/store.js
+++ src/store/store.js
@@ -2,5 +2,14 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
-
+import changeVideo from '../actions/currentVideo.js';
+import changeVideoList from '../actions/videoList.js';
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+const middlewareEnhancer = applyMiddleware(thunk);
+var baseState = {
+  videoList: [],
+  currentVideo: {}
+};
+const store = createStore(rootReducer, baseState, middlewareEnhancer);
+
+export default store;
