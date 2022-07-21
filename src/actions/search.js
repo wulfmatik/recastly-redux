8606e3303ae5b8b4879bc9b60d84fc932b752e09
Blake Crawford <blake.crawford@outlook.com>
2022-07-20 18:33:49 -0700

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -6,13 +6,32 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 var handleVideoSearch = (q) => {
 
-  //TODO:  Write an asynchronous action to handle a video search!
-  var callback = (videos) => {
+  var type = 'HANDLE_VIDEO_SEARCH';
+
+  return ({key, query, max = 5}, callback = (videos) => {
     changeVideoList(videos);
     changeVideo(videos[0]);
-  };
+  }) => {
+    $.get('https://www.googleapis.com/youtube/v3/search', {
+      part: 'snippet',
+      key: key,
+      q: query,
+      maxResults: max,
+      type: 'video',
+      videoEmbeddable: 'true'
+    })
+      .done(({items}) => {
+        if (callback) {
+          callback(items);
+        }
+      })
+      .fail(({responseJSON}) => {
+        responseJSON.error.errors.forEach((err) =>
+          console.error(err)
+        );
+      });
 
-  searchYouTube({YOUTUBE_API_KEY, q}, callback);
+  };
 };
 
 export default handleVideoSearch;
